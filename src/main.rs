d189e94afdba038726760e56639ff9d9e3e3a91a
Will S. Medrano <will.s.medrano@gmail.com>
2023-06-25 19:04:15 -0700

    Instantiate plugin.

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     for (idx, plugin) in world.iter_plugins().enumerate() {
         println!("{}: {}", idx, plugin.name());
     }
+    let plugin = world.iter_plugins().next().unwrap();
+    let features = world.build_features(livi::FeaturesBuilder::default());
+    let _instance = unsafe { plugin.instantiate(features, 44100.0) }.unwrap();
 }
