2c75016fb040edec23088c9e2348737dda665608
Will Medrano <will.s.medrano@gmail.com>
2023-10-17 08:47:27 -0700

    Initialize JACK client.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use anyhow::Result;
 use log::info;
 
-fn main() {
+fn main() -> Result<()> {
     env_logger::builder()
         .filter_level(log::LevelFilter::Info)
         .try_init()
@@ -10,4 +11,12 @@ fn main() {
         std::env::current_dir().unwrap(),
         std::env::args().collect::<Vec<_>>()
     );
+
+    let (client, status) = jack::Client::new("bats", jack::ClientOptions::NO_START_SERVER)?;
+    info!("Started client {} with status {:?}", client.name(), status);
+    let process_handler = ();
+    let client = client.activate_async((), process_handler)?;
+    std::thread::park();
+    client.deactivate()?;
+    Ok(())
 }
