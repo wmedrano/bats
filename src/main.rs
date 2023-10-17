993ff5588bddb589f16c156a7cca7092e3c3a28b
Will Medrano <will.s.medrano@gmail.com>
2023-10-17 08:33:04 -0700

    Add logging.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use log::info;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Info)
+        .try_init()
+        .unwrap();
+    info!(
+        "Current Dir: {:?}, Args: {:?}",
+        std::env::current_dir().unwrap(),
+        std::env::args().collect::<Vec<_>>()
+    );
 }
